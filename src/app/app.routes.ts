603198bc28c23fbf468613c58ec5c7c684dee52c
Demenx <60588359+Cristian339@users.noreply.github.com>
2025-03-08 18:35:29 +0100

    Se hizo la interfaz del inicio y el ordenar palabra por palabras, falta terminar todavia.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
+import { InicioComponent } from './inicio/inicio.component';
 import { Routes } from '@angular/router';
+import { InfantilComponent } from './infantil/infantil.component';
+import { PrimariaComponent } from './primaria/primaria.component';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'infantil', component: InfantilComponent },
+  { path: 'primaria', component: PrimariaComponent },
 ];
